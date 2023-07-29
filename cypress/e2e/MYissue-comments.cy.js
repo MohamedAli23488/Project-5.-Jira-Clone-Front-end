68aa717266b62614cdd223b8f13b1762ad632315
your MohamedAli23488 <moh.ali.234488@gmail.com>
2023-07-29 16:29:12 +0300

    Sprint 2

--- cypress/e2e/MYissue-comments.cy.js
+++ cypress/e2e/MYissue-comments.cy.js
@@ -35,14 +35,43 @@ describe('Issue comments creating, editing and deleting', () => {
             cy.get(Comments)
                 .contains('Delete')
                 .click();
-            })    
-            ///cy.get('[data-testid="modal:confirm"]').contains('button', 'Delete comment')
-                //.click().should('not.exist');
-            cy.get('[data-testid="modal:confirm"]')
-              .contains('button', 'Delete comment')
-             .click()
+        })
+        cy.get('[data-testid="modal:confirm"]')
+            .contains('button', 'Delete comment')
+            .click()
             .should('not.exist');
-            getIssueDetailsModal().find(NewComment).should('not.exist')
+        getIssueDetailsModal().find(NewComment).should('not.exist')
 
-        });
     });
+
+    it('Should create a comment and cancel it', () => {
+        const Mycomment = 'Mohamed comment'
+        const Comments = '[data-testid="issue-comment"]'
+        getIssueDetailsModal().within(() => {
+            cy.contains('Add a comment...').click();
+            cy.get('textarea[placeholder="Add a comment..."]').type(Mycomment);
+            cy.contains('button', 'Cancel').click().should('not.exist');
+            cy.contains('Add a comment...').should('exist');
+            cy.get(Comments).should('not.contain', Mycomment);
+        });
+
+    });
+    const Comments = '[data-testid="issue-comment"]'
+    it.only('Should cancel Deleting a comment', () => {
+        getIssueDetailsModal()
+            .find(Comments)
+            .contains('Delete')
+            .click();
+        cy.get('[data-testid="modal:confirm"]')
+            .contains('button', 'Cancel')
+            .click()
+            .should('not.exist');
+
+        getIssueDetailsModal()
+            .find(Comments)
+            .contains('An old silent pond...')
+            .should('exist');
+    })
+})
+
+
